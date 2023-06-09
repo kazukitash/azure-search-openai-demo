3ef558fef8c4689079d618f3f80a78de6e63e6b3
kazukitash <kazukitash@gmail.com>
2023-06-09 04:43:27 +0000

    update

--- app/backend/approaches/retrievethenread.py
+++ app/backend/approaches/retrievethenread.py
@@ -5,16 +5,16 @@ from azure.search.documents.models import QueryType
 from text import nonewlines
 
 # Cognitive SearchとOpenAIのAPIを直接使用した、シンプルなretrieve-then-readの実装です。これは、最初に
-# 検索からトップ文書を抽出し、それを使ってプロンプトを構成し、OpenAIで補完生成する 
+# 検索からトップ文書を抽出し、それを使ってプロンプトを構成し、OpenAIで補完生成する
 # (answer)をそのプロンプトで表示します。
 
 # Simple retrieve-then-read implementation, using the Cognitive Search and OpenAI APIs directly. It first retrieves
-# top documents from search, then constructs a prompt with them, and then uses OpenAI to generate an completion 
+# top documents from search, then constructs a prompt with them, and then uses OpenAI to generate an completion
 # (answer) with that prompt.
 class RetrieveThenReadApproach(Approach):
 
     template = \
-"あなたは鉄道に関する質問をサポートする教師アシスタントです。" + \
+"あなたは鉄道に関する質問をサポートする案内アシスタントです。" + \
 "質問者が「私」で質問しても、「あなた」を使って質問者を指すようにする。" + \
 "次の質問に、以下の出典で提供されたデータのみを使用して答えてください。" + \
 "各出典元には、名前の後にコロンと実際の情報があり、回答で使用する各事実には必ず出典名を記載します。" + \
@@ -22,14 +22,14 @@ class RetrieveThenReadApproach(Approach):
 """
 
 ###
-Question: '水素ハイブリッド電車とはなんですか?'
+Question: 'スタンプラリーはどこでやっていますか'
 
 Sources:
-info1.txt: 水素をエネルギー源とする燃料電池は、高いエネルギー変換効率と環境負荷の少なさが特徴
-info2.txt: 燃料電池自動車やバスの技術を鉄道車両の技術と融合・応用することにより、水素ハイブリッド電車を開発し、実証試験を始めた。
+info1.txt: ○このたび、開業１２０周年を記念して５月１３日（土）～２１日（日）の９日間、池袋駅に乗り入れる鉄道四社スタンプ ラリーを開催します。
+info2.txt: ≪鉄道四社スタンプラリーin池袋駅の内容≫\n■開業１２０周年を記念し、鉄道四社スタンプラリーｉｎ池袋駅を開催します。イベント期間中、専用台紙に４つの スタンプを集めて台紙をノベルティ引換場所にお持ちいただくと、お好きなノベルティをお一つプレゼントします。
 
 Answer:
-水素を燃料とする燃料電池は、高いエネルギー変換効率と環境負荷の少なさが特徴[info1.txt]です。この技術を鉄道車両に応用し、水素ハイブリッド電車を開発し、実証試験を始めました。[info2.txt] 
+5月13日（土）から21日（日）の9日間で、池袋に乗り入れる鉄道四社スタンプラリーを開催します。[info1.txt]イベント期間中、専用台紙に４つの スタンプを集めて台紙をノベルティ引換場所にお持ちいただくと、お好きなノベルティをお一つプレゼントします。[info2.txt]
 
 ###
 Question: '{q}'?
@@ -53,13 +53,13 @@ Answer:
         filter = "category ne '{}'".format(exclude_category.replace("'", "''")) if exclude_category else None
 
         if overrides.get("semantic_ranker"):
-            r = self.search_client.search(q, 
+            r = self.search_client.search(q,
                                         filter=filter,
-                                        query_type=QueryType.SEMANTIC, 
-                                        query_language="ja-jp", 
-                                        query_speller="none", 
-                                        semantic_configuration_name="default", 
-                                        top=top, 
+                                        query_type=QueryType.SEMANTIC,
+                                        query_language="ja-jp",
+                                        query_speller="none",
+                                        semantic_configuration_name="default",
+                                        top=top,
                                         query_caption="extractive|highlight-false" if use_semantic_captions else None)
         else:
             r = self.search_client.search(q, filter=filter, top=top)
@@ -71,11 +71,11 @@ Answer:
 
         prompt = (overrides.get("prompt_template") or self.template).format(q=q, retrieved=content)
         completion = openai.Completion.create(
-            engine=self.openai_deployment, 
-            prompt=prompt, 
-            temperature=overrides.get("temperature") or 0.3, 
-            max_tokens=1024, 
-            n=1, 
+            engine=self.openai_deployment,
+            prompt=prompt,
+            temperature=overrides.get("temperature") or 0.3,
+            max_tokens=1024,
+            n=1,
             stop=["\n"])
 
         return {"data_points": results, "answer": completion.choices[0].text, "thoughts": f"Question:<br>{q}<br><br>Prompt:<br>" + prompt.replace('\n', '<br>')}
